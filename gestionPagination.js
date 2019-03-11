3d27736f903966079ee7975b4dc57de3d2de1c61
tanguy crepy <tanguycrepy@gmail.com>
2019-03-11 08:41:13 +0100

    FiltreIndex: fix de la conservation de la recherche des checkboxes

--- gestionPagination.js
+++ gestionPagination.js
@@ -212,6 +212,9 @@ GestionPagination.prototype.getPreviousSearch = function () {
         let GestionPagination = this;
         $(GestionPagination.id_search).find(GestionPagination.class_filtre_input).each(function () {
             let input = $(this);
+            if (input.attr('type') === 'checkbox') {
+                input.attr('checked', false);
+            }
             datas.forEach(function (e) {
                 if (e.name === input.attr('name') && e.value === input.val()) {
                     if (input.attr('type') === 'checkbox') {
